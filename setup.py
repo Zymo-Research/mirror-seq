86039e4a843907201e5dee792cca79cf091e89d0
Hunter Chung <b89603112@gmail.com>
2016-04-03 16:50:18 -0700

    draft of setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mirror_seq',
+    version='0.1',
+    description='The bioinformatics tool for Mirror-seq.',
+    url='https://github.com/Zymo-Research/mirror-seq',
+    author='Hunter Chung',
+    author_email='b89603112@gmail.com',
+    licence='Apache License 2.0',
+    packages=['']
+    install_requires=[])
