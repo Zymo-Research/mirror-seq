fd24bb5a7a3b71e14efc5e46ad71df12372984a9
Hunter Chung <hchung@zymoresearch.com>
2016-04-28 09:16:46 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ sudo chmod 777 -R /mnt/
 cd /mnt
 
 wget https://s3.amazonaws.com/zymo-files/mirror-seq/in322_2_ACAGTG_L006_R1_001.fastq.gz
-wget https://s3.amazonaws.com/zymo-files/mirror-seq/in322_2_ACAGTG_L006_R1_001.fastq.gz
+wget https://s3.amazonaws.com/zymo-files/mirror-seq/in322_2_ACAGTG_L006_R2_001.fastq.gz
 docker run --rm -v /mnt/:/mnt anigeo/awscli s3 cp s3://zymo-files/mirror-seq/hg19.tar.gz /mnt/
 tar xvf /mnt/hg19.tar.gz
 docker run -it --rm -v /mnt/:/mnt \
